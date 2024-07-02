c50a1630866e02b2d193d7d63da55d5f24b40df8
lemurlenok <lemurlenok@gmail.com>
2024-07-02 16:39:13 +0300

    hw3

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ToggleComponent, PreviousComponent } from "./hooks/components";
 
-const App: FC = () =>
-    (
-        <>
-
-        </>
-    )
+const App: React.FC = () => {
+    return (
+        <div className="App">
+            <h1>Перевірка</h1>
+            <ToggleComponent />
+            <hr />
+            <PreviousComponent />
+        </div>
+    );
+};
 
 export default App;
---
+++ src/hooks/components.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import useToggle from "../hooks/useToggle";
+import usePrevious from "../hooks/usePrevious";
+
+const ToggleComponent: React.FC = () => {
+    const [isToggled, toggle] = useToggle(false);
+
+    return (
+        <div>
+            <h2>Toggle Component</h2>
+            <button onClick={toggle}>Тисни сюди</button>
+            <p>{isToggled ? "On" : "Off"}</p>
+        </div>
+    );
+};
+
+const PreviousComponent: React.FC = () => {
+    const [count, setCount] = useState<number>(-20);
+    const previousCount = usePrevious(count); 
+
+    return (
+        <div>
+            <h2>Previous Component</h2>
+            <button onClick={() => setCount(count + 1)}>Тиць</button>
+            <p>Current: {count} </p>
+            <p>Previous: {previousCount !== undefined ? previousCount : "null"}</p>
+        </div>
+    );
+};
+
+export { ToggleComponent, PreviousComponent };

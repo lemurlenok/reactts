f6e7bbc68e7689726cd95c0a68b74948a092b50f
lemurlenok <lemurlenok@gmail.com>
2024-07-08 16:43:33 +0300

    hw5

--- src/validators/userValidator.tsx
+++ src/validators/userValidator.tsx
@@ -3,11 +3,11 @@ import Joi from "joi";
 const userValidator =
     Joi.object({
         userId: Joi.number()
-            .min(3)
+            .min(1)
             .max(9)
             .required()
             .messages({
-                "number.min": "min value is 3",
+                "number.min": "min value is 1",
                 "number.max": "max value is 9"
             }),
 
@@ -16,14 +16,14 @@ const userValidator =
             .required()
             .messages({
                 "string.empty": "title cannot be empty",
-                "string.pattern.base": "title cannot be shorter than 7 characters"
+                "string.pattern.base": "title must contain 7 characters"
             }),
         body: Joi.string()
             .required()
             .pattern(/^.{8,}$/)
             .messages({
                 "string.empty": "this area cannot be empty",
-                "string.pattern.base": "title cannot be shorter than 8 characters"
+                "string.pattern.base": "up to 8 characters"
             })
     })
 
